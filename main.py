d8bb1751460792c6bf2a829ced3cc9577d84b16d
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-08-05 13:53:40 +0530

    feature extraction completed

--- main.py
+++ main.py
@@ -1,12 +1,64 @@
 from src.logger import logger
 from src.exceptions import CustomException
-logger.info('Hello')
 import sys
+from src.utils import load_pkl , dump_pkl
+from src.extract_features import (extract_features , 
+                                   model)
 
-if __name__=="__main__":
-    try:
-        a = 1/0
-        # raise CustomException
-    except Exception as e:
-        logger.info('ZERO Division ERRor')
-        raise CustomException(e,sys)
+
+
+img_path = load_pkl(file_path='model/img_files_path.pkl')
+
+# images_features_from_model = feature_extractor(img_path=img_path,model=model)
+
+# img_features = extract_features(imgs_file_paths=img_path,model=model)
+
+# dump_pkl(obj=img_features,file_path='model/img_features.pkl')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__=="__main__":
+#     try:
+#         a = 1/0
+#         # raise CustomException
+#     except Exception as e:
+#         logger.info('ZERO Division ERRor')
+#         raise CustomException(e,sys)
--- src/extract_features.py
+++ src/extract_features.py
@@ -3,36 +3,43 @@ from keras_vggface.utils import preprocess_input
 from keras.preprocessing import image
 import numpy as np
 import cv2
-import pickle
+
 import tqdm
 from src.exceptions import CustomException
 import sys
-from src.utils import load_pkl
 import warnings
 warnings.filterwarnings('ignore')
+from src.logger import logger
 
 model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
 
-imgs_file_paths = load_pkl(file_path='model/img_files_path.pkl')
+
 def feature_extractor(img_path , model):
     img_array = cv2.resize(cv2.imread(filename=img_path),dsize=((224,224))).astype(np.float32) # Here I just reading the image and resizing it.
-  
+    # logger.info(f'converted the pictures to images')
+
     expanded_img = np.expand_dims(img_array,axis=0) # Insert a new axis that will appear at the axis position in the expanded array shape.
 
     preprocessed_img = preprocess_input(expanded_img) #This function preprocesses the input image array according to the requirements of the specific pre-trained model
+    # logger.info(f'Preprocessing using [preprocess_input] method has done.')
 
-    return model.predict(preprocessed_img) # This method takes the input data, performs forward propagation through the model, and generates predictions for the input data
+    return model.predict(preprocessed_img) ,  # This method takes the input data, performs forward propagation through the model, and generates predictions for the input data
+
+
+def extract_features(imgs_file_paths , model):
+    imgs_features = []  # making the features list .
+
+    try:
+        for file in tqdm.tqdm(imgs_file_paths):
+            imgs_features.append(feature_extractor(img_path=file,model=model))
+
+        logger.info(f'appended all the imgs_features')
+        return imgs_features
+    
+        
+    except Exception:
+        raise CustomException(Exception , sys)
 
 
 
-imgs_features = []  # making the features list .
-
-# print(imgs_features)
-# imgs_features.clear()
-
-# try:
-#     for file in tqdm.tqdm(imgs_file_paths):
-#         imgs_features.append(feature_extractor(img_path=file,model=model))
-# except Exception:
-#     raise CustomException(Exception , sys)
     
