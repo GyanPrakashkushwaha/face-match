aa820ae12cc1634233f30737505ae7af8c20036a
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-08-05 14:40:10 +0530

    deleted features.pkl

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from src.utils import load_pkl , dump_pkl
 from src.extract_features import (extract_features , 
                                    model)
 
-from src.recommend import feature_list , file_names
+# from src.recommend import feature_list , file_names
 
 img_path = load_pkl(file_path='model/img_files_path.pkl')
 
 # images_features_from_model = feature_extractor(img_path=img_path,model=model)
 
-# img_features = extract_features(imgs_file_paths=img_path,model=model)
+img_features = extract_features(imgs_file_paths=img_path,model=model)
 
-# dump_pkl(obj=img_features,file_path='model/img_features.pkl')
-
-
+dump_pkl(obj=img_features,file_path='model/img_features.pkl')
 
 
-print(feature_list)
-print(len(file_names))
+
+
+# print(feature_list)
+# print(len(file_names))
 
 
 
